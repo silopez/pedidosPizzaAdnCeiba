6ecbb246a15600490d2ddb2fe7249e506f66bbf5
CEIBA\alexander.lopez <alexander.lopez@ceiba.com.co>
2021-01-25 07:42:30 -0500

    Primer commit para el ADN ceiba

---
+++ src/main/java/com/pedidospizza/adnceiba/PedidosPizzaAdnCeibaApplication.java
@@ -0,0 +1,13 @@
+package com.pedidospizza.adnceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosPizzaAdnCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosPizzaAdnCeibaApplication.class, args);
+	}
+
+}
