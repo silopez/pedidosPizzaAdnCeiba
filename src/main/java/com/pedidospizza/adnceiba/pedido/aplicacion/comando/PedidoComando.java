f110b17b60944a010e28026b20e4ad2db0858571
CEIBA\alexander.lopez <alexander.lopez@ceiba.com.co>
2021-02-01 08:13:30 -0500

    Correccion PedidoComando

--- src/main/java/com/pedidospizza/adnceiba/pedido/aplicacion/comando/PedidoComando.java
+++ src/main/java/com/pedidospizza/adnceiba/pedido/aplicacion/comando/PedidoComando.java
@@ -18,12 +18,12 @@ public class PedidoComando {
 	
 	public PedidoComando() {}
 
-	public PedidoComando(Long id, String nombre, String direccion, Integer total, EstadoPedidoEnum estadoPedido, Date fechaPedido) {
+	public PedidoComando(Long id, String nombre, String direccion, Integer total, EstadoPedidoEnum estadoPedido, Date fechaPedido2) {
 		this.id = id;
 		this.nombre = nombre;
 		this.direccion = direccion;
 		this.total = total;
 		this.estadoPedido = estadoPedido;
-		this.fechaPedido = fechaPedido;
+		this.fechaPedido = fechaPedido2;
 	}
 }
